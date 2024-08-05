536c232c6612d77eedbcbeac367871ec856b53f4
IsraelAraujo70 <israelaraujodeoliveira@gmail.com>
2024-08-05 14:55:48 -0300

    Responsividade finalizada e toques adicionais

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,3 @@
-
 document.addEventListener('mousemove', function(e) {
     const light = document.querySelector('.light');
     const mouseX = e.clientX;
@@ -6,4 +5,41 @@ document.addEventListener('mousemove', function(e) {
 
     light.style.setProperty("--mouse-x", `${mouseX}px`);
     light.style.setProperty("--mouse-y", `${mouseY}px`);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const sections = document.querySelectorAll('section');
+    const navLi = document.querySelectorAll('.header__nav__item');
+
+    window.addEventListener('scroll', () => {
+        let current = '';
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+            if (pageYOffset >= sectionTop - sectionHeight / 3) {
+                current = section.getAttribute('id');
+            }
+        });
+
+        navLi.forEach(li => {
+            li.classList.remove('active');
+            if (li.querySelector('a').getAttribute('href').includes(current)) {
+                li.classList.add('active');
+            }
+        });
+    });
+
+    const menuButton = document.getElementById('menuButton');
+    const navMenu = document.getElementById('navMenu');
+    const header = document.getElementById('header');
+
+    menuButton.addEventListener('click', function() {
+        navMenu.classList.toggle('show');
+        if (header.style.borderBottom === '3px solid transparent') {
+            header.style.borderBottom = '3px solid var(--underLine)';
+        } else {
+            header.style.borderBottom = '3px solid transparent';
+        }
+    });
 });
