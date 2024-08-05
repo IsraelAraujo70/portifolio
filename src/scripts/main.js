d69c8e7316944c41c1ca96572e903ee03554a284
IsraelAraujo70 <israelaraujodeoliveira@gmail.com>
2024-08-05 16:10:31 -0300

    Removed nav watcher

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -8,38 +8,17 @@ document.addEventListener('mousemove', function(e) {
 });
 
 document.addEventListener('DOMContentLoaded', function() {
-    const sections = document.querySelectorAll('section');
-    const navLi = document.querySelectorAll('.header__nav__item');
-
-    window.addEventListener('scroll', () => {
-        let current = '';
-
-        sections.forEach(section => {
-            const sectionTop = section.offsetTop;
-            const sectionHeight = section.clientHeight;
-            if (pageYOffset >= sectionTop - sectionHeight / 3) {
-                current = section.getAttribute('id');
-            }
-        });
-
-        navLi.forEach(li => {
-            li.classList.remove('active');
-            if (li.querySelector('a').getAttribute('href').includes(current)) {
-                li.classList.add('active');
-            }
-        });
-    });
-
     const menuButton = document.getElementById('menuButton');
     const navMenu = document.getElementById('navMenu');
     const header = document.getElementById('header');
 
     menuButton.addEventListener('click', function() {
         navMenu.classList.toggle('show');
-        if (header.style.borderBottom === '3px solid transparent') {
+        if (header.style.borderBottom === 'none') {
             header.style.borderBottom = '3px solid var(--underLine)';
         } else {
-            header.style.borderBottom = '3px solid transparent';
+            header.style.borderBottom = 'none';
         }
     });
 });
+
